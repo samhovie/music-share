595458b6abac00ab4aec6b33a43a2ca0181eea51
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-05 15:26:52 -0700

    basic tables

---
+++ backend/app/models/song.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Song(db.Model):
+    __tablename__ = "songs"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    # artist_name = db.Column(db.String(255), nullable = False)
+    # artist_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    # album_id = db.Column(db.Integer, nullable = False)
+    # album_name = db.Column(db.String(100), nullable = False)
+    mp3_file = db.Column(db.String(255), nullable = False)
+    genre = db.Column(db.String(255), nullable = False)
+    # song_position = db.Column(db.String(100), nullable = False)
+    created_at =db.Column(db.DateTime, nullable = False)
+    updated_at = db.Column(db.DateTime, nullable = False)
+
+    # user = db.relationship('Users', back_populates = 'songs', cascade='all, delete-orphan')
+    # comments = db.relationship('Comments', back_populates = 'song')

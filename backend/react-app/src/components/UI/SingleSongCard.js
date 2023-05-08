0849f5ae0ee162b56dc52dc0da81b6a9cf343ce7
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-05-08 15:51:29 -0500

    more css styling finished

--- backend/react-app/src/components/UI/SingleSongCard.js
+++ backend/react-app/src/components/UI/SingleSongCard.js
@@ -6,39 +6,67 @@ const SingleSongCard = () => {
         <>
             <div className='single-song-card-wrapper'>
                 <div className='single-song-card-image'>
-
+                    <img src='https://external-preview.redd.it/MY3_HQFLzswJrX8tYEEbBuodnWH67nqf5gDYSZrFh0s.jpg?auto=webp&s=c75ba2d2994db81df63721b8da0af2316dd3df86'></img>
                 </div>
                 <div className='single-song-card-info'>
                     <div className='single-song-card-info-top'>
-                        <div className='single-song-card-play-button'>
-
+                        <div className='single-song-card-info-top-left-column'>
+                            <div className='single-song-card-play-button'>
+                                Play Button
+                            </div>
+                            <div className='single-song-card-next-to-play'>
+                                <div className='single-song-card-artist'>
+                                    Artist
+                                </div>
+                                <div className='single-song-card-song'>
+                                    Song
+                                </div>
+                            </div>
                         </div>
-                        <div className='single-song-card-artist'>
-
+                        {/* <div > */}
+                        <div className='single-song-card-info-top-right-column'>
+                            Genre
                         </div>
-                        <div className='single-song-card-song'>
-
-                        </div>
-                    </div>
-                    <div className='single-song-card-play-button'>
+                        {/* </div> */}
                     </div>
                     <div className='single-song-card-info-soundwave'>
-
+                        <p>
+                            -------------------------------------------------------------
+                        </p>
                     </div>
                     <div className='single-song-card-info-comment'>
-
+                        <form>
+                            <input
+                            className='single-song-card-info-comment-input'
+                            placeholder='Write a comment'></input>
+                        </form>
                     </div>
                     <div className='single-song-card-info-bottom'>
                         <div className='single-song-card-info-bottom-left-column'>
-                            <div className='single-song-card-info-bottom-left-column-heart'>H</div>
-                            <div className='single-song-card-info-bottom-left-column-repost'>R</div>
-                            <div className='single-song-card-info-bottom-left-column-share'>S</div>
-                            <div className='single-song-card-info-bottom-left-column-copy-link'>L</div>
-                            <div className='single-song-card-info-bottom-left-column-ellipsis'>E</div>
+                            <div className='single-song-card-info-bottom-left-column-heart'>
+                                <i>heart</i>
+                                Heart
+                            </div>
+                            <div className='single-song-card-info-bottom-left-column-repost'>
+                                <i>repost</i>
+                                Repost
+                            </div>
+                            <div className='single-song-card-info-bottom-left-column-share'>
+                                <i>repost</i>
+                                Share Link
+                            </div>
+                            <div className='single-song-card-info-bottom-left-column-copy-link'>
+                                <i>copy link</i>
+                                Copy Link
+                            </div>
+                            <div className='single-song-card-info-bottom-left-column-ellipsis'>
+                                <i>ellipsis</i>
+                                Ellipsis
+                            </div>
                         </div>
                         <div className='single-song-card-info-bottom-right-column'>
-                            <div className='single-song-card-info-bottom-right-column-plays'>P</div>
-                            <div className='single-song-card-info-bottom-right-column-comments'>C</div>
+                            <div className='single-song-card-info-bottom-right-column-plays'>Plays</div>
+                            <div className='single-song-card-info-bottom-right-column-comments'>Comments</div>
                         </div>
                     </div>
 

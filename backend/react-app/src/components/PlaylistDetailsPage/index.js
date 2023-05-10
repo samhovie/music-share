32bfd05df360a9fb051aa85e9ddd9994133206d9
Katie Geyer <keg233@nau.edu>
2023-05-10 13:16:08 -0700

    replaced playlist css with song css

--- backend/react-app/src/components/PlaylistDetailsPage/index.js
+++ backend/react-app/src/components/PlaylistDetailsPage/index.js
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { addSongToPlaylistThunk, getPlaylistThunk } from "../../store/playlists";
 import PlaylistCard from "../UI/PlaylistCard";
+import SingleSongCard from "../UI/SingleSongCard";
 import GetPlaylistUsername from "./GetPlaylistUsername.js"
 
 
@@ -57,27 +58,27 @@ function PlaylistDetailsPage() {
                                     Add Song to Playlist (in modal)
                                 </button> */}
                             </div>
-                            <div className='playlist-details-page-display-likes'>
+                            <div className='song-details-page-display-likes'>
                                 Like Count
                             </div>
                         </div>
                         {/* <div> */}
-                        <div className='playlist-details-page-profile-songs'>
-                            <div className='playlist-details-page-user'>
-                                <div className='playlist-details-page-user-image'>
+                        <div className='song-details-page-profile-songs'>
+                            <div className='song-details-page-artist'>
+                                <div className='song-details-page-artist-image'>
                                     <img
                                         src='https://resizing.flixster.com/eU7-Qa3193jrUTIth9yZM3DdsF4=/218x280/v2/https://flxt.tmsimg.com/assets/761404_v9_aa.jpg'
                                     ></img>
                                 </div>
                                 <p
-                                    className='playlist-details-page-user-name'
+                                    className='song-details-page-user-name'
                                 >
                                     <GetPlaylistUsername></GetPlaylistUsername>
                                     {/* {`${playlist.playlist.user}`} */}
                                 </p>
 
                             </div>
-                            <div className='playlist-details-page-display-songs-each'>
+                            <div className='song-details-page-display-songs-each'>
                                 {/* for each song in playlist, render <SongInPlaylist /> passing in a song prop */}
                             </div>
                         </div>

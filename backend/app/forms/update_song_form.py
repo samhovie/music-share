6ba89cc9b0fb37ce5027354de47c659e05630606
Samuel Hovie <samhovie@umich.edu>
2023-05-15 11:27:21 -0400

    m

---
+++ backend/app/forms/update_song_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from app.aws import ALLOWED_EXTENSIONS
+from wtforms.validators import DataRequired, Email, ValidationError
+from app.models import User
+
+
+class UpdateSongForm(FlaskForm):
+    name = StringField('Title', validators=[DataRequired()])
+    artist_name = StringField('Artist', validators=[DataRequired()])
+    mp3_file = StringField('File', validators=[DataRequired()])
+    # mp3_file = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    genre = StringField('Genre', validators=[DataRequired()])

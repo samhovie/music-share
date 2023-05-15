6039bc257b9ad83a34f491b5fdec90e620405730
Samuel Hovie <samhovie@umich.edu>
2023-05-14 22:15:37 -0400

    route fix attempt

--- backend/app/api/songs_routes.py
+++ backend/app/api/songs_routes.py
@@ -35,28 +35,15 @@ def get_all_songs():
 
 @songs_routes.route('/new', methods=['POST'])
 def post_songs():
-    # print("TEST 3333333333333")
     form = SongForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    # if request.method == "GET":
-    #     return form
-    # print(form.data)
-    # print('request', request.files)
     if form.validate_on_submit():
-        # print("TEST 1", form.data['songs'])
         if "mp3_file" not in request.files:
             return {"errors": "song file required"}, 400
         image = request.files["mp3_file"]
-        # print("TEST 2", image)
         image.filename = get_unique_filename(image.filename)
         upload = upload_file_to_s3(image)
-        # upload = ''
 
-        # with open(image) as file:
-        #     upload = upload_file_to_s3(file)
-
-        # print("REQUEST.FILES", request.files)
-        # print(request.files['name'])
         if "url" not in upload:
             # print("UPLOAD[ERRORS]", upload['errors'])
         # if the dictionary doesn't have a url key
@@ -66,15 +53,15 @@ def post_songs():
             return upload, 400
         url = upload["url"]
 
-        preview_img_file = request.files["preview_img"]
-        preview_img_file.filename = get_unique_filename(
-            preview_img_file.filename)
-        preview_img_upload = upload_file_to_s3(preview_img_file)
+        # preview_img_file = request.files["preview_img"]
+        # preview_img_file.filename = get_unique_filename(
+        #     preview_img_file.filename)
+        # preview_img_upload = upload_file_to_s3(preview_img_file)
 
-        if "url" not in preview_img_upload:
-            return preview_img_upload, 400
+        # if "url" not in preview_img_upload:
+        #     return preview_img_upload, 400
 
-        preview_img_url = preview_img_upload["url"]
+        # preview_img_url = preview_img_upload["url"]
 
         new_song = Song(
             name=form.data['name'],
@@ -82,7 +69,8 @@ def post_songs():
             mp3_file=url,
             genre=form.data['genre'],
             artist_id=current_user.id,
-            preview_img=preview_img_url,
+            # preview_img=preview_img_url,
+            preview_img=''
             created_at=date.today(),
             updated_at=date.today()
         )

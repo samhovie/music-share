4b65feae53586d3040087d72f5b56a27a55b5144
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-06-13 12:56:04 -0500

    messing with carousel

--- backend/react-app/src/components/UI/Carousel/Carousel.js
+++ backend/react-app/src/components/UI/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './Carousel.css'
 
 const CarouselComponent = () => {
 

c01d873eacdafce7742e15bd96c672d654d59ccf
Samuel Hovie <samhovie@umich.edu>
2023-06-26 02:18:44 -0400

    seed progress

--- backend/react-app/src/components/DiscoverPage/index.js
+++ backend/react-app/src/components/DiscoverPage/index.js
@@ -1,40 +1,68 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllPlaylistsThunk } from '../../store/playlists';
-import PlaylistWrapper from '../UI/PlaylistWrapper';
-import './DiscoverPage.css'
-import OpenModalButton from '../OpenModalButton';
-import CreatePlaylistForm from '../CreateNewPlaylist';
-import CarouselComponent from '../UI/Carousel/Carousel';
-// import {CarouselComponent} from '../UI/Carousel/Carousel';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPlaylistsThunk } from "../../store/playlists";
+import OpenModalButton from "../OpenModalButton";
+import CreatePlaylistForm from "../CreateNewPlaylist";
+import PlaylistCard from "../UI/PlaylistCard";
+import "./DiscoverPage.css";
 
 
 const DiscoverPage = () => {
-    const dispatch = useDispatch()
-    const allPlaylists = useSelector((state) => state.playlists.allPlaylists)
-    const playlists = Object.values(allPlaylists)
+    const dispatch = useDispatch();
+    const allPlaylists = useSelector((state) => state.playlists.allPlaylists);
+    const playlists = Object.values(allPlaylists);
 
     useEffect(() => {
-        dispatch(getAllPlaylistsThunk())
-    }, [dispatch])
+        dispatch(getAllPlaylistsThunk());
+    }, [dispatch]);
 
     return (
         <>
-            <div className='global-outerwrapper-outer'>
-                <div className='global-outerwrapper-wrapper discover-page-wrapper'>
-                    <CarouselComponent />
-                    <OpenModalButton
-                        className='buttonss'
-                        modalComponent={<CreatePlaylistForm />}
-                        buttonText="Create Playlist"
-                    />
+            <div className="discover-page-wrapper">
+                <OpenModalButton
+                    className="buttonss"
+                    modalComponent={<CreatePlaylistForm />}
+                    buttonText="Create Playlist"
+                />
 
-                    <PlaylistWrapper playlists={playlists} />
+                <div className="playlist-wrapper-wrapper">
+                    <h2 className="playlist-wrapper-title">Latest</h2>
+                    <div className="playlist-wrapper-inner-wrapper">
+                        {playlists.map((playlist) => (
+                            <PlaylistCard
+                                playlist={playlist}
+                                key={playlist.id}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="playlist-wrapper-wrapper">
+                    <h2 className="playlist-wrapper-title">Hip-hop</h2>
+                    <div className="playlist-wrapper-inner-wrapper">
+                        {playlists.map((playlist) => (
+                            <PlaylistCard
+                                playlist={playlist}
+                                key={playlist.id}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="playlist-wrapper-wrapper">
+                    <h2 className="playlist-wrapper-title">Rock</h2>
+                    <div className="playlist-wrapper-inner-wrapper">
+                        {playlists.map((playlist) => (
+                            <PlaylistCard
+                                playlist={playlist}
+                                key={playlist.id}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
-
         </>
-    )
-}
+    );
+};
 
-export default DiscoverPage
+export default DiscoverPage;
